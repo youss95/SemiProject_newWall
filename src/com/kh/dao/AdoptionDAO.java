80f1eea71b807021e97e04c077fd8e75029359c7
eelsoras2 <eelsoras2@gmail.com>
2021-07-01 14:27:53 +0900

    update files

--- src/com/kh/dao/AdoptionDAO.java
+++ src/com/kh/dao/AdoptionDAO.java
@@ -11,6 +11,7 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 import com.kh.dto.AdoptionDTO;
+import com.kh.dto.AnimalDTO;
 
 public class AdoptionDAO {
 
@@ -32,7 +33,36 @@ public class AdoptionDAO {
 		return ds.getConnection();
 	}
 	
+	public List<AnimalDTO> getAnimalList() throws Exception{
+		String sql = "select * from animals";
 
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery();
+				){
+			List<AnimalDTO> list = new ArrayList<>();
+			while(rs.next()) {
+				AnimalDTO dto = new AnimalDTO();
+				
+				dto.setCode_seq(rs.getNString("code_seq"));
+				dto.setAn_name(rs.getNString("an_name"));
+				dto.setAn_category(rs.getNString("an_category"));
+				dto.setAn_gender(rs.getNString("an_gender"));
+				dto.setAn_kind(rs.getNString("an_kind"));
+				dto.setAn_age(rs.getInt("an_age"));
+				dto.setAn_weight(rs.getInt("an_weight"));
+				dto.setAn_character(rs.getNString("an_character"));
+				dto.setAn_date(rs.getDate("an_date"));
+				dto.setAn_status(rs.getNString("an_status"));
+				dto.setAn_contents(rs.getNString("an_contents"));
+				dto.setAn_reg_date(rs.getDate("an_reg_date"));
+
+				list.add(dto);
+			}
+			return list;
+		}
+	}
 	
 	
 	
