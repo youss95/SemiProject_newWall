cc89eedd2a50a2ca4424a17953bfe85760d7a4d8
youss95 <usekim7555@gmail.com>
2021-07-01 14:33:26 +0900

    임시보호 폼

--- WebContent/resources/js/animal/imagePreview.js
+++ WebContent/resources/js/animal/imagePreview.js
@@ -16,3 +16,17 @@ function imageChoose(obj){
 	reader.readAsDataURL(f)
 	
 }
+
+function imageChoose2(obj){
+	let f = obj.files[0];
+	if(!f.type.match("image.*")){
+		alert("이미지를 등록해야 됨");
+		return;
+	}
+	let reader = new FileReader();
+	reader.onload = (e) =>{
+		$("#imageUploadPreview2").attr('src',e.target.result);
+	}
+	reader.readAsDataURL(f)
+	
+}
