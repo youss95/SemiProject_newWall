849d5fcaf87bb8e7672c27fae6981b993dc392f1
eelsoras2 <eelsoras2@gmail.com>
2021-07-09 15:33:37 +0900

    filecontroller 수정

--- src/com/kh/controller/FileController.java
+++ src/com/kh/controller/FileController.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("*.file")
+@WebServlet("*.sfile")
 public class FileController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
